d3a09b5f5a80385c20328e4071e74222a86d13e3
Fred Yankowski <fcy@imsa.edu>
2016-03-25 12:37:15 -0500

    New benchmark of several dot-product implementations.

--- bench/Bench.elm
+++ bench/Bench.elm
@@ -5,6 +5,7 @@ import Signal
 import Benchmark
 import Task exposing (Task, andThen)
 import Text
+import Math.Vector2 as V2 exposing (Vec2, vec2)
 import Math.Vector3 as V3 exposing (Vec3, vec3)
 import Math.Matrix4 as M4
 
@@ -39,6 +40,31 @@ mat4Suite =
       [ Benchmark.bench1 "M4.mul" driver () ]
 
 
+dotSuite =
+  let
+    dotTuple ( x1, y1 ) ( x2, y2 ) =
+      (x1 * x2) + (y1 * y2)
+
+    dotSimple x1 y1 x2 y2 =
+      (x1 * x2) + (y1 * y2)
+
+    driverTuple _ =
+      dotTuple ( 10, 20 ) ( 30, 40 )
+
+    driverSimple _ =
+      dotSimple 10 20 30 40
+
+    driver _ =
+      V2.dot (V2.Vec2 10 20) (V2.Vec2 30 40)
+  in
+    Benchmark.Suite
+      "Dot suite"
+      [ Benchmark.bench1 "tuple dot" driverTuple ()
+      , Benchmark.bench1 "simple dot" driverSimple ()
+      , Benchmark.bench1 "V2.dot" driver ()
+      ]
+
+
 results : Signal.Mailbox String
 results =
   Signal.mailbox "Benchmark loading"
@@ -46,4 +72,4 @@ results =
 
 port benchResults : Task Benchmark.Never ()
 port benchResults =
-  Benchmark.runWithProgress (Just results) mat4Suite `andThen` (\_ -> Task.succeed ())
+  Benchmark.runWithProgress (Just results) dotSuite `andThen` (\_ -> Task.succeed ())
